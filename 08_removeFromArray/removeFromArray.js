bfde9469a8fb6453fed36d9a586f361ce2089868
elyas-get <psudoelyas@gmail.com>
2025-07-25 19:31:25 +0300

    intial commit

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, removeFromArray) {
+    if (!Array.isArray(array)) {
+        throw new TypeError('Input must be an array');
+  }
+  const updated = array.filter(n => n !== removeFromArray)
+  return updated
 };
-
+console.log(removeFromArray([1,2,3,4,5,6,6],6))
 // Do not edit below this line
 module.exports = removeFromArray;
