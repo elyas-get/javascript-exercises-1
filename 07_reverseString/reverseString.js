8ef3b8a5925e76b1b2c0667fef9596369e94ca73
elyas-get <psudoelyas@gmail.com>
2025-07-25 19:21:55 +0300

    final commit

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let reversed = ''
+    for (i= text.length-1; i >=0; i--){
+        reversed += text[i]
+    }
+    return reversed
 };
-
+console.log(reverseString('Elyas'))
 // Do not edit below this line
 module.exports = reverseString;
