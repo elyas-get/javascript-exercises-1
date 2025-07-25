70eb3048046582c5d13ff4d0e99a9ea527ffc744
elyas-get <psudoelyas@gmail.com>
2025-07-25 21:40:21 +0300

    intial commit

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeFara) {
+  return ((degreeFara-32)*5)/9 + ' C'
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeCels) {
+  return (degreeCels * (9/5)) + 32 + ' F'
 };
 
+console.log(convertToCelsius(32)) // fahrenheit to celsius, should return 0
+
+console.log(convertToFahrenheit(0)) // celsius to fahrenheit, should return 32
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
