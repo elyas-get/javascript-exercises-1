6a057f8d63c5f5c0c8abffa6565ff6686ff7aefb
elyas-get <psudoelyas@gmail.com>
2025-07-25 19:14:17 +0300

    intial commit

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(text,num) {
+    let result = ''
+    for(i=0; i<num; i++){
+        result += text
+    }
+    return result
 };
 
 // Do not edit below this line
