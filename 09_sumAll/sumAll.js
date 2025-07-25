d70b71d5ce5b0919b7b1b01e8dce1d133fe918f5
elyas-get <psudoelyas@gmail.com>
2025-07-25 19:37:09 +0300

    intial commit

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(x,y) {
+    let total = 0
+    if (x > y){
+        for (i=y; i <= x; i++){
+            total += i
+        }
+    }
+    else{
+        for (i=x; i <= y; i++){
+            total += i
+        } 
+    }
+    return total
 };
+console.log(sumAll(1,4))
 
 // Do not edit below this line
 module.exports = sumAll;
