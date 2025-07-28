5e1e803dcbbf9c6180d4669252adb2552d10d3d1
elyas-get <psudoelyas@gmail.com>
2025-07-28 12:58:16 +0300

    final commit

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  const total = array.reduce((sum,item) => sum + item, 0)
+  return total
 };
 
-const multiply = function() {
-
+const multiply = function(a,b) {
+  return a * b
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return a ** b	
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0 || a === 1) {
+    return 1;
+  } else {
+    return a * factorial(a - 1);
+  }
 };
 
 // Do not edit below this line
