8551f75515ebd4fa201770a43aec8c50f129dc9e
elyas-get <psudoelyas@gmail.com>
2025-07-28 13:10:23 +0300

    intial commit

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (text) {
+  const specials = [' ', '-', ';', ',', '.', '!', '?'];
+  let result = "";
 
+  for (let i = 0; i < text.length; i++) {
+    const char = text[i].toLowerCase();
+    if (!specials.includes(char)) {
+      result += char;
+    }
+  }
+
+  const reversed = result.split("").reverse().join("");
+
+  return result === reversed;
 };
 
 // Do not edit below this line
