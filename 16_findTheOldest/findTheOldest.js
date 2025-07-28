49c580879b93316c3ae4468e8e88d069de6b0b0b
elyas-get <psudoelyas@gmail.com>
2025-07-28 21:43:09 +0300

    inital commit

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const people = [
+    {
+    name: "Carly",
+    yearOfBirth: 2018,
+    },
+    {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+    },
+    {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+    },
+]
 
+const findTheOldest = function(array) {
+    const oldest = array.sort((a,b)=>{
+    let x = a.yearOfDeath - a.yearOfBirth
+    let y = b.yearOfDeath - b.yearOfBirth
+    return y - x
+    })
+    return oldest[0]
 };
 
 // Do not edit below this line
