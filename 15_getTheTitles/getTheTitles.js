0c59d8a460562e3432180d808f045a7278b538df
elyas-get <psudoelyas@gmail.com>
2025-07-28 15:55:57 +0300

    final commit

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
 
+const getTheTitles = function(array) {
+    let titles = []
+    for (i=0;i<array.length; i++){
+        titles.push(array[i].title)
+    }
+    return titles
 };
 
+console.log(getTheTitles(books))
 // Do not edit below this line
 module.exports = getTheTitles;
